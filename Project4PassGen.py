d1b6bb9d0d3c4fa9303bca87c0074adf657ef0be
Soham Joshi <91250124+ManInTheHam@users.noreply.github.com>
2024-01-15 10:26:32 +0530

    Update Project4PassGen.py

--- Project4PassGen.py
+++ Project4PassGen.py
@@ -38,8 +38,7 @@ def generate_password(min_length, numbers=True, special_characters=True):
 
 min_length = input("Enter the Minimum Length: ")
 has_number = input("Do you want to have numbers (y/n)?").lower() == "y"
-has_special = input(
-    "Do you want to have special characters (y/n)?").lower() == "y"
+has_special = input("Do you want to have special characters (y/n)?").lower() == "y"
 pwd = generate_password(min_length, has_number, has_special)
 generate_password(10)
 print(pwd)
